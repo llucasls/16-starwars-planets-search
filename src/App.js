39f13fc17a2419ffc5c37ed1930485b8871d7cae
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-09-20 14:53:45 -0300

    Criado o componente Table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Table from './components/Table';
 
 function App() {
+  const [ data, setData ] = useState({});
+  const linkAPI = 'https://swapi-trybe.herokuapp.com/api/planets/'
+  fetch(linkAPI)
+    .then((value) => (value.json))
+    .then((value) => (setData(value.results)));
+  console.log(data);
   return (
-    <span>Hello, App!</span>
+    Table(data)
   );
 }
 
---
+++ src/components/Table.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+function Table(data) {
+  // console.log(data);
+  return (
+    <span>Hello, App!</span>
+  );
+}
+
+export default Table;
