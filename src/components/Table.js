4cf88c132184b66db0774ede55fbade3e57c92a3
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-10-11 12:58:16 -0300

    Nomes das colunas modificados

--- src/components/Table.js
+++ src/components/Table.js
@@ -8,19 +8,19 @@ function Table() {
       <table>
         <thead>
           <tr>
-            <th>name</th>
-            <th>rotation_period</th>
-            <th>orbital_period</th>
-            <th>diameter</th>
-            <th>climate</th>
-            <th>gravity</th>
-            <th>terrain</th>
-            <th>surface_water</th>
-            <th>population</th>
-            <th>films</th>
-            <th>created</th>
-            <th>edited</th>
-            <th>url</th>
+            <th>Planet Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
           </tr>
         </thead>
         <tbody>
