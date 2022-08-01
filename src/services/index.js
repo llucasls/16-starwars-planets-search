c015650aa02323e38fca528f3ce6180554fe4c37
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-08-01 11:23:30 -0300

    refactor: remove commented code

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,3 @@
-// const filterPlanets = (planetList, filters) => {
-//   const { name } = filters.filterByName;
-//   return planetList
-//     .filter((planet) => (
-//       planet.name.toLowerCase().includes(name.toLowerCase())
-//     ));
-// };
-
 const removeUsedColumns = (columns, filters) => {
   const remainingColumns = [];
   const { filterByNumericValues } = filters;
@@ -14,10 +6,7 @@ const removeUsedColumns = (columns, filters) => {
       if (
         columns.includes(column)) {
         remainingColumns.concat(column);
-        // const index = columns.indexOf(column);
-        // (columns.slice(0, index).concat(columns.slice(index + 1)));
       }
-      // console.log(column);
     });
   return columns;
 };
@@ -31,27 +20,5 @@ const order = (prev, next) => {
     prev < next ? -(ONE) : (ONE)
   );
 };
-/*   useEffect(() => {
-    const filteredPlanets = data
-      .filter((planet) => (
-        planet.name.includes(filters.filterByName.name)
-      ));
-    let planetList = filteredPlanets;
-    filters.filterByNumericValues.forEach(({
-      comparison, column, value,
-    }) => {
-      if (comparison === 'maior que') {
-        planetList = planetList.filter((planet) => planet[column] > Number(value));
-      }
-      if (comparison === 'menor que') {
-        planetList = planetList.filter((planet) => planet[column] < Number(value));
-      }
-      if (comparison === 'igual a') {
-        planetList = planetList.filter((planet) => planet[column] === (value));
-      }
-      return planetList;
-    });
-    setFilteredData(planetList);
-  }, [data, filters]); */
 
 export { removeUsedColumns, order };
